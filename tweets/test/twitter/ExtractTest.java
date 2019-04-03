7b6b870a5f79c9f8223020b19b87551fdd914c6d
haris <haris@DESKTOP-53LECQL>
2019-04-04 00:22:23 +0500

    Assignment has been done

--- tweets/test/twitter/ExtractTest.java
+++ tweets/test/twitter/ExtractTest.java
@@ -26,7 +26,7 @@ public class ExtractTest {
     
     private static final Tweet tweet1 = new Tweet(1, "alyssa", "is it reasonable to talk about rivest so much?", d1);
     private static final Tweet tweet2 = new Tweet(2, "bbitdiddle", "rivest talk in 30 minutes #hype", d2);
-    private static final Tweet tweet3 = new Tweet(2, "hashaam", "how are you#hype", d2);
+    private static final Tweet tweet3 = new Tweet(2, "haris", "how are you#hype", d2);
     private static final Tweet tweet4= new Tweet(2, "alyssa", "fine#hype", d2);
     
     @Test(expected=AssertionError.class)
